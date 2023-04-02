e8bc93940865eda84b390eab5bf0a661d3b81364
Vasilij U <vasya-1001@yandex.ru>
2023-04-02 18:58:39 +0500

    Example 003 Sum added

---
+++ Ex003_Sum/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Clear();
+int a = new Random().Next(1,100) ;
+Console.Write("a = ");
+Console.WriteLine(a);
+int b = new Random().Next(1,100) ;
+Console.Write("b = ");
+Console.WriteLine(b);
+int sum = a+b;
+Console.Write("sum = ");
+Console.WriteLine(sum);
