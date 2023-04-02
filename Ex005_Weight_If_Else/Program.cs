49b1a57d5a88e60078d452f137e86a4606f3d8a2
Vasilij U <vasya-1001@yandex.ru>
2023-04-02 20:42:24 +0500

    Example 005 Weight If - Else added

---
+++ Ex005_Weight_If_Else/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+int ind = 0;
+int max = 0;
+Console.Write("Введите количество гирь: ");
+//string numbers = 
+int numbers = Convert.ToInt32(Console.ReadLine());
+int[] Giri = new int [numbers];
+while (ind < numbers)
+{
+    Giri[ind] = new Random().Next(1,1000);
+    if (Giri[ind]>max) max = Giri[ind];
+    Console.WriteLine(Giri[ind]);
+    ind = ind + 1;
+}
+Console.Write("max = ");
+Console.WriteLine(max);
