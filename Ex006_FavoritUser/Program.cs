ffd15f1409b50216aec9b66f9a2c1266c2515bf8
Vasilij U <vasya-1001@yandex.ru>
2023-04-02 20:53:50 +0500

    Example 006 Favorit User added

---
+++ Ex006_FavoritUser/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Clear();
+Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+if (username.ToLower() == "вася")
+{
+    Console.WriteLine("Ура, это же Вася!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
