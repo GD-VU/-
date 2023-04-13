10c2b9777cf30f0190a1497ae562d4dc40802504
Vasilij U <vasya-1001@yandex.ru>
2023-04-13 20:55:43 +0500

    Example 008 Intro Method Modification one folder added

---
+++ Ex008_IntroMethodMod1/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Clear();
+
+int 
+    a1 = 1,
+    b1 = 2,
+    c1 = 3,
+    a2 = 4,
+    b2 = 5,
+    c2 = 6,
+    a3 = 7,
+    b3 = 8,
+    c3 = 9;
+
+int max = a1;
+
+if (b1 > max) max = b1;
+if (c1 > max) max = c1;
+
+if (a2 > max) max = a2;
+if (b2 > max) max = b2;
+if (c2 > max) max = c2;
+
+if (a3 > max) max = a3;
+if (b3 > max) max = b3;
+if (c3 > max) max = c3;
+
+Console.WriteLine("Проверка выполнения программы: " + max);
